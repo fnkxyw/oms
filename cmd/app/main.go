df2aad3ff98e79847285b8a3e04360e7041b4eac
Anton Kushn <akugnerevich@gmail.com>
2024-09-05 16:05:42 +0300

    [GOJHW-1]: add functions:returnOrder, placeOrder, listOrders; update storage; add sygnal.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"gitlab.ozon.dev/akugnerevich/homework-1.git/cmd/sygnal"
 	"gitlab.ozon.dev/akugnerevich/homework-1.git/internal/basic"
 	"gitlab.ozon.dev/akugnerevich/homework-1.git/internal/cli"
+	"gitlab.ozon.dev/akugnerevich/homework-1.git/internal/storage"
 	"os"
 )
 
 func main() {
+	strg := storage.NewOrderStorage()
+	err := strg.ReadFromJSON("data/orders.json")
+	if err != nil {
+		return
+	}
+	sygnal.SygnalSearch(strg)
 	if len(os.Args) > 1 {
 		cli.Execute()
 	} else {
-		basic.Run()
+		basic.Run(strg)
 	}
 }
