12cf7dc80d9e45e45544509627679dc4dde85903
Anton Kushn <akugnerevich@gmail.com>
2024-09-28 00:57:08 +0300

    [GOJHW-4]: update migrations

--- migrations/20240927211845_create_index_refunded.sql
+++ migrations/20240927211845_create_index_refunded.sql
@@ -1,7 +1,6 @@
 -- +goose Up
-CREATE INDEX idx_orders_state_refunded ON orders(state)
-    WHERE state = 'refunded';
-
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_orders_state_refunded ON orders(state)
+WHERE state = 'refunded';
 
 -- +goose Down
-DROP INDEX idx_orders_state_refunded;
+DROP INDEX CONCURRENTLY IF EXISTS idx_orders_state_refunded;
--- migrations/20240927212940_create_index_userID.sql
+++ migrations/20240927212940_create_index_userID.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-CREATE INDEX idx_orders_user_id ON orders(user_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_orders_user_id ON orders(user_id);
 
 -- +goose Down
-DROP INDEX idx_orders_user_id;
+DROP INDEX CONCURRENTLY IF EXISTS idx_orders_user_id;
