986fbca14e7a1b72d6b61ade7c0e28eba1696be9
Anton Kushn <akugnerevich@gmail.com>
2024-09-28 14:13:58 +0300

    [GOJHW-4]: update migrations

--- migrations/20240927212940_create_index_userID.sql
+++ migrations/20240927212940_create_index_userID.sql
@@ -1,5 +1,7 @@
 -- +goose Up
+-- +goose NO TRANSACTION
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_orders_user_id ON orders(user_id);
 
 -- +goose Down
+-- +goose NO TRANSACTION
 DROP INDEX CONCURRENTLY IF EXISTS idx_orders_user_id;
