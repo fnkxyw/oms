43ce1cb65d2bd288254c2feedf88cf5d0f64caae
Anton Kushn <akugnerevich@gmail.com>
2024-10-28 19:18:49 +0300

    [GOJHW]-8: fix cache usage

--- internal/storage/facade.go
+++ internal/storage/facade.go
@@ -73,25 +73,9 @@ func (s storageFacade) AcceptOrder(ctx context.Context, or *models.Order) error 
 
 func (s storageFacade) PlaceOrder(ctx context.Context, ids []uint32) error {
 	return s.txManager.RunReadCommited(ctx, func(ctxT context.Context) error {
-		var orders []models.Order
-		var missingIDs []uint32
-		for _, id := range ids {
-			if order, found := s.cache.Get(strconv.Itoa(int(id))); found {
-				orders = append(orders, *order.(*models.Order))
-			} else {
-				missingIDs = append(missingIDs, id)
-			}
-		}
-
-		if len(missingIDs) > 0 {
-			dbOrders, exists := s.PgRepo.GetItems(ctxT, missingIDs)
-			if !exists {
-				return e.ErrNoConsist
-			}
-			for _, order := range dbOrders {
-				s.cache.Set(strconv.Itoa(int(order.ID)), &order, 30*time.Second)
-				orders = append(orders, order)
-			}
+		orders, exists := s.PgRepo.GetItems(ctxT, ids)
+		if !exists {
+			return e.ErrNoConsist
 		}
 		baseUserID := orders[0].UserID
 		for _, order := range orders {
@@ -114,6 +98,7 @@ func (s storageFacade) PlaceOrder(ctx context.Context, ids []uint32) error {
 		if err := s.PgRepo.UpdateBeforePlace(ctxT, ids, time.Now()); err != nil {
 			return err
 		}
+
 		err := s.producer.SendMessages(ctx, orders, models.PlaceEvent)
 		if err != nil {
 			return ErrLogEvent
@@ -122,20 +107,8 @@ func (s storageFacade) PlaceOrder(ctx context.Context, ids []uint32) error {
 	})
 }
 
-func (s storageFacade) ReturnOrder(ctx context.Context, id uint) error {
+func (s storageFacade) ReturnOrder(ctx context.Context, order *models.Order) error {
 	return s.txManager.RunReadCommited(ctx, func(ctxTx context.Context) error {
-		var order *models.Order
-		if cachedOrder, t := s.cache.Get(strconv.Itoa(int(id))); t {
-			order = cachedOrder.(*models.Order)
-		} else {
-			var exists bool
-			order, exists = s.PgRepo.GetItem(ctxTx, id)
-			if !exists {
-				return e.ErrNoConsist
-			}
-			s.cache.Set(strconv.Itoa(int(order.ID)), order, 5*time.Second)
-		}
-
 		err := order.CanBeReturned()
 		if err != nil {
 			return err
@@ -148,6 +121,32 @@ func (s storageFacade) ReturnOrder(ctx context.Context, id uint) error {
 		s.cache.Remove(strconv.Itoa(int(order.ID)))
 
 		err = s.producer.SendMessage(ctx, *order, models.ReturnEvent)
+		if err != nil {
+			return ErrLogEvent
+		}
+
+		return nil
+	})
+}
+
+func (s storageFacade) RefundOrder(ctx context.Context, order *models.Order, userId uint) error {
+	return s.txManager.RunReadCommited(ctx, func(ctxTx context.Context) error {
+		if order.State != models.PlaceState {
+			return e.ErrNotPlace
+		}
+		if time.Now().After(order.PlaceDate.AddDate(0, 0, 2)) {
+			return e.ErrTimeExpired
+		}
+		if order.UserID != userId {
+			return e.ErrIncorrectUserId
+		}
+
+		err := s.PgRepo.UpdateState(ctxTx, order.ID, models.RefundedState)
+		if err != nil {
+			return err
+		}
+
+		err = s.producer.SendMessage(ctx, *order, models.RefundEvent)
 		if err != nil {
 			return ErrLogEvent
 		}
@@ -182,46 +181,6 @@ func (s storageFacade) ListOrders(ctx context.Context, id uint, inPuP bool, coun
 	s.cache.Set(cacheKey, list, 5*time.Second)
 
 	return list, nil
-}
-
-func (s storageFacade) RefundOrder(ctx context.Context, id uint, userId uint) error {
-	return s.txManager.RunReadCommited(ctx, func(ctxTx context.Context) error {
-		var order *models.Order
-		var exists bool
-
-		if cachedOrder, found := s.cache.Get(strconv.Itoa(int(id))); found {
-			order = cachedOrder.(*models.Order)
-		} else {
-			order, exists = s.PgRepo.GetItem(ctxTx, id)
-			if !exists {
-				return e.ErrCheckOrderID
-			}
-		}
-
-		if order.State != models.PlaceState {
-			return e.ErrNotPlace
-		}
-		if time.Now().After(order.PlaceDate.AddDate(0, 0, 2)) {
-			return e.ErrTimeExpired
-		}
-		if order.UserID != userId {
-			return e.ErrIncorrectUserId
-		}
-
-		err := s.PgRepo.UpdateState(ctxTx, id, models.RefundedState)
-		if err != nil {
-			return err
-		}
-
-		s.cache.Set(strconv.Itoa(int(order.ID)), order, 5*time.Second)
-
-		err = s.producer.SendMessage(ctx, *order, models.RefundEvent)
-		if err != nil {
-			return ErrLogEvent
-		}
-
-		return nil
-	})
 }
 
 func (s storageFacade) ListReturns(ctx context.Context, limit, page int) ([]models.Order, error) {
